5218a3c5ae4669d3256bd198eac5f6368ef9b535
mskim <mskimsid@gmail.com>
2026-06-25 08:57:21 +0900

    feat(table-preview): log backtrace on preview render failure (parity with PreviewService)

--- app/controllers/design/table_style_previews_controller.rb
+++ app/controllers/design/table_style_previews_controller.rb
@@ -17,6 +17,7 @@ module Design
       head :not_found
     rescue => e
       Rails.logger.error("[design] table-style preview failed: #{e.class}: #{e.message}")
+      Rails.logger.error(e.backtrace.first(5).join("\n")) if e.backtrace
       head :unprocessable_entity
     end
   end
